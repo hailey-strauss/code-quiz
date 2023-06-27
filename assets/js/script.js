91e1caa1ee552d32c16513f933e80e356326a1d4
Hailey Strauss <hstrauss2001@gmail.com>
2023-06-26 22:16:23 -0400

    created a placeholder for choices and questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
 var startbtn = document.querySelector("#startbtn");
 function startquiz() {
   console.log("Hello");
+  questions.textContent = "you have made it";
+  for (let index = 0; index < 4; index++) {
+    var choice = document.createElement("button");
+    choice.textContent = index;
+    choices.appendChild(choice);
+  }
 }
-
+var questions = document.getElementById("question-text");
+var choices = document.getElementById("choices");
 startbtn.addEventListener("click", startquiz);
