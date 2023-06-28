3b85e0b4d1666b5ac4d9ea1b386be45238d52a2d
Hailey Strauss <hstrauss2001@gmail.com>
2023-06-27 21:49:58 -0400

    added an array of questions for the quiz as well as what I can do for the future.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,59 @@
 var startbtn = document.querySelector("#startbtn");
+var myArray = [
+  {
+    question: "What makes you most happy?",
+    choiceA: "music",
+    choiceB: "building",
+    choiceC: "structure",
+    choiceD: "iPhone",
+    correctAnswer: "music",
+  },
+  {
+    question: "What makes you most happy?",
+    choiceA: "music",
+    choiceB: "building",
+    choiceC: "structure",
+    choiceD: "iPhone",
+    correctAnswer: "music",
+  },
+  {
+    question: "What makes you most happy?",
+    choiceA: "music",
+    choiceB: "building",
+    choiceC: "structure",
+    choiceD: "iPhone",
+    correctAnswer: "music",
+  },
+  {
+    question: "What makes you most happy?",
+    choiceA: "music",
+    choiceB: "building",
+    choiceC: "structure",
+    choiceD: "iPhone",
+    correctAnswer: "music",
+  },
+  {
+    question: "What makes you most happy?",
+    choiceA: "music",
+    choiceB: "building",
+    choiceC: "structure",
+    choiceD: "iPhone",
+    correctAnswer: "music",
+  },
+];
 function startquiz() {
   console.log("Hello");
-  questions.textContent = "you have made it";
-  for (let index = 0; index < 4; index++) {
-    var choice = document.createElement("button");
-    choice.textContent = index;
-    choices.appendChild(choice);
-  }
+  questions.textContent = myArray[0].question;
+  // for (let index = 0; index < 4; index++) {
+  //   var choice = document.createElement("button");
+  //   choice.textContent = index;
+  //   choices.appendChild(choice);
+  // }
 }
 var questions = document.getElementById("question-text");
 var choices = document.getElementById("choices");
 startbtn.addEventListener("click", startquiz);
+// To do Items: create a timer option and seperate (function). Need variable to track first question
+// Function to write and retreive from local storage. And complete loop inside of startquiz
+// grab the current question
+// console.log(myArray[0].choiceA); example of how to access each question
